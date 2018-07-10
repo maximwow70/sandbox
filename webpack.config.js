477193e8fd54474274d805153d4faac4c59bab32
Maksim Samuilionak <maksim_samuilionak@epam.com>
2018-07-10 15:17:28 +0300

    Fix issue with resolve files.

--- webpack.config.js
+++ webpack.config.js
@@ -107,7 +107,10 @@ module.exports = env => {
                     test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
                     loader: "file-loader",
                     options: {
-                        outputPath: __dirname + "/dist/assets/"
+                        outputPath: "./assets/", // relative to dist
+                        name(file) {
+                            return isDevelopment ? "[name].[ext]" : "[hash].[ext]";
+                        }
                     }
                 },
                 {
@@ -149,7 +152,7 @@ module.exports = env => {
                     ignore: ["index.html"]
                 },
                 { from: "./src/index.html", to: "./index.html" },
-                { from: "assets/**/*", to: "./", context: "./src/" } // todo: replace with file loader
+                // { from: "assets/**/*", to: "./", context: "./src/" } // todo: replace with file loader
             ], {})
             // new LiveReloadPlugin()
         ],
